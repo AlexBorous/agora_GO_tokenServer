405b49d474ee3287e15631affe4ffc2da9a0850d
AlexBorous <alexborousas@gmail.com>
2021-03-06 17:58:07 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"strconv"
 	"time"
 
@@ -31,9 +33,20 @@ func main() {
 	api.GET("chat_integration/:appID/:token/", setToken)
 	api.GET("rtm/:uid/", getRtmToken)
 	api.GET("rte/:channelName/:role/:tokentype/:uid/", getBothTokens)
-	api.Run(":8080") // listen and serve on localhost:8080
+	err := http.ListenAndServe(GetPort(), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
-
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
 func nocache() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// set headers
